52cf10c566d1b1c2ab6cc7c63cb357c67c957927
Cori <f.benayas@upm.es>
2018-07-31 14:22:47 +0200

    Cambios minimos de formato y subo el video para facilitar ruta

--- toponew.py
+++ toponew.py
@@ -9,11 +9,11 @@ from mininet.node import RemoteController, Controller
 import os
 
 
-class SingleSwitchTopo(Topo):
+class SingleSwitchTopo(Topo):exit
+
     def build(self, n=2):
         switch = self.addSwitch('s1')
 	
-
         for h in range(n):
             host = self.addHost('h%s' % (h + 1))
             self.addLink(host, switch)
@@ -28,24 +28,18 @@ def simpleTest():
     dumpNodeConnections(net.hosts)
     print "Let's do a pingAll..."
     net.pingAll()
-    h1 = net.get('h1') 
-    h2 = net.get('h2')
-
-
-	
-    #cmd = 'su bayesiansdn -c "vlc /home/bayesiansdn/vlcprueba/small.mp4"'
-    #el host no tiene conexión, la prueba de youtube no funciona
-    #cmd = 'su bayesiansdn -c "vlc https://youtu.be/AsGUJpCCkVw"'
-    # TO DO : Hacemos que h1 mande el video small y que h2 lo reciba con xterm.
-    cmd1 = "su bayesiansdn ;"
-    cmd1 = "vlc-wrapper -vvv /home/bayesiansdn/bayesiansdn-demos/network-simulator/mininet/net/samples/SampleVideo_1280x720_30mb.mkv --sout '#duplicate{dst=rtp{dst=10.0.0.2,port=5004,mux=ts},dst=display}'"
+    src = net.get('h1') 
+    dst = net.get('h2')
     
-    #client side
-    cmd2 = "su bayesiansdn ;"
-    cmd2 = "vlc-wrapper rtp://10.0.0.2:5004"
+    #Server side
+    cmdServer = "su bayesiansdn ;"
+    cmdServer = "vlc-wrapper -vvv sampleVideo.mkv --sout '#duplicate{dst=rtp{dst=10.0.0.2,port=5004,mux=ts},dst=display}'"
+    #Client side
+    cmdClient = "su bayesiansdn ;"
+    cmdClient = "vlc-wrapper rtp://10.0.0.2:5004"
     
-    miterm = makeTerm(h2, title='VLC Client', term='xterm', display=None, cmd=cmd2)
-    miterm = makeTerm(h1, title='VLC Server', term='xterm', display=None, cmd=cmd1)
+    termDst = makeTerm(dst, title='VLC Client', term='xterm', display=None, cmd=cmdClient)
+    termSrc = makeTerm(src, title='VLC Server', term='xterm', display=None, cmd=cmdServer)
     
     CLI(net)
     
