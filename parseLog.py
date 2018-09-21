2a859f0e8d77691b725f9b4c9f7c87794bd3a73e
Cori <ccabreral@axpe.com>
2018-09-21 09:13:08 +0200

    Rewriting comments and format

--- parseLog.py
+++ parseLog.py
@@ -13,7 +13,6 @@ def  fileExists():
 	    file = open(fn, 'w')
 	return file   
 
-
 list_of_files_server = glob.glob('/home/bayes/Repositories/pruebas/logs/server*') # * means all if need specific format then *.csv
 list_of_files_client = glob.glob('/home/bayes/Repositories/pruebas/logs/client*')
 latest_file_server = max(list_of_files_server, key=os.path.getctime)
@@ -24,42 +23,29 @@ print latest_file_server
 
 # Open the file that contains all information about sessions.
 file = fileExists()
-
-
-
-session=subprocess.check_output('grep -m1 "Session*" '+latest_file_client+' | cut -d " " -f4 | cut -d ";" -f1', shell=True)
-#print session
-
+# Get the session ID created by VLC (in logs)
+session = subprocess.check_output('grep -m1 "Session*" '+latest_file_client+' | cut -d " " -f4 | cut -d ";" -f1', shell=True)
 file.write(" Session = "+str(session))
 
-
-
-#variables to get only specific information.
+# Variables to get only specific information.
 read = False
-# counter and numberlines I want to read.
+# Counter and numberlines I want to read.
 counter = 0
 numberLines=30
 
 with open(latest_file_client, 'r') as filehandle:  
     for line in filehandle:
-
-    	
-
-    	#if to get only the number of lines specified in the variable after the DESCRIBE response
-
+    	# If to get only the number of lines specified in the variable after the DESCRIBE response
     	if read :
     		counter += 1
-    		#print contador
-    		#print(line)
-
-    		#if to get the date of the streaming session
+    		# If to get the timestamp of the streaming session
     		if "Date" in line:
     			file.write(" Date = "+ line.split(": ")[1])
 
     		if counter > numberLines :
     			read = False
 
-
+		# Recollecting the data under the DESCRIBE response section 
     	if "DESCRIBE response" in line :
     		read = True
         	#print(line)
