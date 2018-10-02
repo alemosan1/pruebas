bc928a6afc66a077d1fcb97629c3add6c7c6422d
Cori <ccabreral@axpe.com>
2018-10-02 13:00:24 +0200

    anadido todos los campos del 2parselog en elastic y otras mejoras de codigo

--- 2parseLog.py
+++ 2parseLog.py
@@ -7,11 +7,10 @@ list_of_files_server = glob.glob('/home/bayes/Repositories/pruebas/logs/server*'
 list_of_files_client = glob.glob('/home/bayes/Repositories/pruebas/logs/client*')
 latest_file_server = max(list_of_files_server, key=os.path.getctime)
 latest_file_client = max(list_of_files_client, key=os.path.getctime)
-
-session = subprocess.check_output('grep -m1 "Session*" '+latest_file_client+' | cut -d " " -f4 | cut -d ";" -f1', shell=True).rstrip('\n')
+id_logFile = re.findall(r'\d+', latest_file_client)[0]
 
 def  fileExists():
-	fn = "infoSession/infosession"+session+".log"
+	fn = "infoSession/infosession"+id_logFile+".log"
 	try:
 	    file = open(fn, 'a')
 	except IOError:
@@ -19,50 +18,57 @@ def  fileExists():
 	return file   
 
 file = fileExists()
-file.write("Session=" + session + ", ")
+session = subprocess.check_output('grep -m1 "Session*" '+latest_file_client+' | cut -d " " -f4 | cut -d ";" -f1', shell=True).rstrip('\n')
 
 with open(latest_file_client, 'r') as filehandle:  
-	# Pillamos la primera linea que contiene la IP del cliente
+	channels = samplerate = bitrate = acodec = "NotApplicable" # Generamos estas variables aunque el audio no se reproduzca
+	
+	# IP CLIENT
 	line = filehandle.readline()
 	ip = line.split("=  ")
-	file.write("IPClient=" + ip[1].rstrip('\n') + ", ")
+	ip_client = "IPClient=" + ip[1].rstrip('\n')
 
 	for line in filehandle:
-		if "Content-Base:" in line :
+		if "Content-Base:" in line : # IP SERVER
 			ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line)
-			file.write("IPServer=" + ip[0] + ", ")
+			ip_server = "IPServer=" + ip[0]
 			#file.write(date)
 		#if "Date" in line:
 			#date = "Quitar el dia de la semana" + line.split(": ")[1]
-		if "Transport:" in line :
+		if "Transport:" in line : # PORTS
 			if "client_port" and "server_port" in line :
 				line = line.split(";")
-				file.write(line[2] + " " + line [3] + ", ")
+				ports = line[2] + ", " + line [3]
 
-		# Audio info
+		# AUDIO INFO
 		if "samplerate:" in line :
-			file.write ("AUDIO: ")
-			array=(line.split("] ")[2]).split("g: ")[1].split(" ")
-			array[3] = array[3].rstrip('\n')
-			for i in array :
-				if ":" not in i  :
-					file.write("acodec=")
-				i=i.replace(':','=')
-				file.write(i)
-				file.write(", ")
-		
-#Server side
-file.write ("VIDEO: ")
+			audioInfo = (line.split("] ")[2]).split("g: ")[1].split(" ")
+			audioInfo[3] = audioInfo[3].rstrip('\n')
+			aux = 1
+			for i in audioInfo :
+				if ":" in i :
+					audioInfo [aux] = i.split(":")[1]
+					aux += 1
+			acodec = audioInfo[0]
+			channels = audioInfo[1]
+			samplerate = audioInfo[2]
+			bitrate = audioInfo[3]
+file.write("Session=" + session + ", " + ip_client + ", " + ip_server + ", " + ports + ", ")
+file.write("AUDIO: " + "acodec="+acodec+ ", channels="+channels+ ", samplerate="+samplerate+ ", bitrate="+bitrate+ ", ")		
+
+#### SERVER SIDE ####
 with open(latest_file_server, 'r') as filehandle:  
-	fps = "fps=NotApplicable" # Ponemos esto para tener esta variable, aunque el video no se reproduzca
-	video2 = "vcodec=NotApplicable"
+	fps_src = fps_dst = vcodec = scale = "NotApplicable" # Generamos estas variables aunque el video no se reproduzca
 	for line in filehandle:
 		if "source fps" in line:
-			line= line.split(": ")[1].split(", ")
-			fps = line[0]+", " + line[1]
+			line = line.split(": ")[1].split(", ")
+			fps_src = line[0].split("=")[1]
+			fps_dst = line[1].split("=")[1]
 		if "core stream output debug: usi" in line:
-			video=line.split("{")[1].split(',')
-			video2 = video[0]+", "+video[1]
+			videoParams=line.split("{")[1].split(',')
+			vcodec = videoParams[0].split("=")[1]
+			scale = videoParams[1].split("=")[1]
 
-	file.write (fps + ", " + video2 + "\n")
+file.write ("VIDEO: " + "fps_src="+fps_src+ ", fps_dst="+fps_dst+ ", vcodec="+vcodec+ ", scale="+scale+ "\n")
+
 # NOTA: El ultimo parametro que se anada al fichero del este log, debe tener un salto de linea. Si no, el filebeat no lo coge.
